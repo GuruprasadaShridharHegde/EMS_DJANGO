0703ffe6db63b69e7cfb91ea45a4ab32ee499758
Guruprasada Shridhar Hegde <85961223+GuruprasadaShridharHegde@users.noreply.github.com>
2023-07-11 22:00:53 +0530

    Update settings.py

--- EMS/Employee_Manage/Employee_Manage/settings.py
+++ EMS/Employee_Manage/Employee_Manage/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-kqiiz3vul-^481&6#&gikn=5=_g9p-oic0_q_842uolyqc#_e@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yourdomain.com', 'localhost', '3.110.43.37']
 
 
 # Application definition
